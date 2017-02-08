13c519c525a1b6d3a504e2eb3bb0f257e0790ea6
Tyler Bennett <tylerbennett215@gmail.com>
2017-02-08 15:37:39 -0500

    Added Service that detects the given activitiies, and also broadcasts an recieves an intent

--- app/src/main/java/com/mylesspencertyler/project3activities/MainActivity.java
+++ app/src/main/java/com/mylesspencertyler/project3activities/MainActivity.java
@@ -1,13 +1,86 @@
 package com.mylesspencertyler.project3activities;
 
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.ActivityRecognition;
+import com.mylesspencertyler.project3activities.service.activityrecognition.ActivityRecognizedService;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    public GoogleApiClient mApiClient;
+    boolean mIsReceiverRegistered = false;
+    ActivityBroadcastReceiver mReceiver = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mApiClient = new GoogleApiClient.Builder(this)
+                .addApi(ActivityRecognition.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+        mApiClient.connect();
+    }
+
+    @Override
+    protected void onResume() {
+        if (!mIsReceiverRegistered) {
+            if (mReceiver == null)
+                mReceiver = new ActivityBroadcastReceiver();
+            registerReceiver(mReceiver, new IntentFilter("YourIntentAction"));
+            mIsReceiverRegistered = true;
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        if (mIsReceiverRegistered) {
+            unregisterReceiver(mReceiver);
+            mReceiver = null;
+            mIsReceiverRegistered = false;
+        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Intent intent = new Intent( this, ActivityRecognizedService.class );
+        PendingIntent pendingIntent = PendingIntent.getService( this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT );
+        ActivityRecognition.ActivityRecognitionApi.requestActivityUpdates( mApiClient, 3000, pendingIntent );
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    private void updateUI(ActivityType result) {
+
+    }
+
+    private class ActivityBroadcastReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            ActivityType result = ActivityType.detachFrom(intent);
+            updateUI(result);
+        }
     }
 }
--- app/src/main/java/com/mylesspencertyler/project3activities/service/activityrecognition/ActivityRecognizedService.java
+++ app/src/main/java/com/mylesspencertyler/project3activities/service/activityrecognition/ActivityRecognizedService.java
@@ -2,14 +2,27 @@ package com.mylesspencertyler.project3activities.service.activityrecognition;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.util.Log;
+
+import com.google.android.gms.location.ActivityRecognitionResult;
+import com.google.android.gms.location.DetectedActivity;
+import com.mylesspencertyler.project3activities.ActivityType;
+
+import java.util.Date;
+import java.util.List;
 
 /**
  * Created by tyler on 2/7/2017.
  */
 
 public class ActivityRecognizedService extends IntentService {
+    private int mCurrentActivity;
+
     public ActivityRecognizedService() {
         super("ActivityRecognizedService");
+        mCurrentActivity = DetectedActivity.UNKNOWN;
     }
 
     public ActivityRecognizedService(String name) {
@@ -18,5 +31,83 @@ public class ActivityRecognizedService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
+        if(ActivityRecognitionResult.hasResult(intent)) {
+            ActivityRecognitionResult result = ActivityRecognitionResult.extractResult(intent);
+            handleDetectedActivities( result.getProbableActivities() );
+        }
+    }
+
+    private void handleDetectedActivities(List<DetectedActivity> probableActivities) {
+        for( DetectedActivity activity : probableActivities ) {
+            switch( activity.getType() ) {
+                case DetectedActivity.IN_VEHICLE: {
+                    Log.e( "ActivityRecogition", "In Vehicle: " + activity.getConfidence() );
+                    if(activity.getConfidence() >= 70 && mCurrentActivity != DetectedActivity.IN_VEHICLE){
+                        Intent intent = new Intent();
+                        Date activityStarted = new Date();
+                        intent.putExtra("timeStarted", activityStarted.getTime());
+                        ActivityType.VEHICLE.attachTo(intent);
+
+                        intent.setAction("com.mylesspencertyler.ACTIVITY_INTENT");
+                        sendBroadcast(intent);
+                        mCurrentActivity = DetectedActivity.IN_VEHICLE;
+                    }
+                    break;
+                }
+
+                case DetectedActivity.RUNNING: {
+                    Log.e( "ActivityRecogition", "Running: " + activity.getConfidence() );
+                    if(activity.getConfidence() >= 70 && mCurrentActivity != DetectedActivity.RUNNING){
+                        Intent intent = new Intent();
+                        Date activityStarted = new Date();
+                        intent.putExtra("timeStarted", activityStarted.getTime());
+                        ActivityType.RUNNING.attachTo(intent);
+
+                        intent.setAction("com.mylesspencertyler.ACTIVITY_INTENT");
+                        sendBroadcast(intent);
+                    }
+                    break;
+                }
+                case DetectedActivity.STILL: {
+                    Log.e("ActivityRecogition", "Still: " + activity.getConfidence());
+                    if(activity.getConfidence() >= 70 && mCurrentActivity != DetectedActivity.STILL){
+                        Intent intent = new Intent();
+                        Date activityStarted = new Date();
+                        intent.putExtra("timeStarted", activityStarted.getTime());
+                        ActivityType.STILL.attachTo(intent);
+
+                        intent.setAction("com.mylesspencertyler.ACTIVITY_INTENT");
+                        sendBroadcast(intent);
+                    }
+                    break;
+                }
+                case DetectedActivity.WALKING: {
+                    Log.e( "ActivityRecogition", "Walking: " + activity.getConfidence() );
+                    if(activity.getConfidence() >= 70 && mCurrentActivity != DetectedActivity.WALKING){
+                        Intent intent = new Intent();
+                        Date activityStarted = new Date();
+                        intent.putExtra("timeStarted", activityStarted.getTime());
+                        ActivityType.WALKING.attachTo(intent);
+
+                        intent.setAction("com.mylesspencertyler.ACTIVITY_INTENT");
+                        sendBroadcast(intent);
+                    }
+                    break;
+                }
+                case DetectedActivity.UNKNOWN: {
+                    Log.e( "ActivityRecogition", "Unknown: " + activity.getConfidence() );
+                    if(activity.getConfidence() >= 70 && mCurrentActivity != DetectedActivity.UNKNOWN){
+                        Intent intent = new Intent();
+                        Date activityStarted = new Date();
+                        intent.putExtra("timeStarted", activityStarted.getTime());
+                        ActivityType.UNKNOWN.attachTo(intent);
+
+                        intent.setAction("com.mylesspencertyler.ACTIVITY_INTENT");
+                        sendBroadcast(intent);
+                    }
+                    break;
+                }
+            }
+        }
     }
 }
